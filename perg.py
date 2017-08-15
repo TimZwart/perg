4e4e0140eec37f888e4d1b340a73baf7f08ce675
Tim Zwart <Tim.Zwart@vixion.nl>
2017-08-15 16:34:39 +0200

    added possibility to exclude dirs from search results

--- perg.py
+++ perg.py
@@ -7,19 +7,31 @@ parser.add_argument("directory")
 parser.add_argument("searchterm")
 arguments = parser.parse_args();
 
+script_dir = os.path.dirname(os.path.realpath(__file__))
+exclude_filename = "exclude.perg"
+
+exclude_filepath =  "/".join([script_dir, exclude_filename])
+with open(exclude_filepath) as exclude_file :
+    excludes = exclude_file.readlines()
+
 def walk_folder(folder, searchterm):
     for (dirpath, dirnames, filenames) in os.walk(folder):
         for filename in filenames:
-            try:
-                filepath = "/".join([dirpath , filename])
-                opened = open(filepath)
-                str_f = opened.read()
-                idx = str_f.find(searchterm)
-                if(idx != -1):
-                    print "match found at", filepath
-            except UnicodeDecodeError:
-                print "nontext file"
-            except IOError:
-                print "IO error"
+            dirs = dirpath.split("/")
+            intersec = [x for x in dirs if x in excludes]
+            if intersec == []:
+                try:
+                    filepath = "/".join([dirpath , filename])
+                    opened = open(filepath)
+                    str_f = opened.read()
+                    idx = str_f.find(searchterm)
+                    if(idx != -1):
+                        print "match found at", filepath
+                except UnicodeDecodeError:
+                    print "nontext file"
+                except IOError:
+                    print "IO error"
+            elif intersec is None:
+                print "error intersection list is None"
 
 walk_folder(arguments.directory, arguments.searchterm)
