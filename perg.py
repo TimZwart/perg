5a393d3505241777404e90dce7cf64029cc67b1a
Tim Zwart <Tim.Zwart@vixion.nl>
2018-09-03 13:38:52 +0200

    first try zip and filename search

--- perg.py
+++ perg.py
@@ -2,7 +2,9 @@
 import os
 import argparse
 import pdb
+import zipfile
 from functools import reduce
+from binaryornot.check import is_binary
 
 parser = argparse.ArgumentParser()
 parser.add_argument("directory")
@@ -15,14 +17,16 @@ exclude_filename = "exclude.perg"
 exclude_filepath =  "/".join([script_dir, exclude_filename])
 with open(exclude_filepath) as exclude_file :
     exclude_lines = exclude_file.readlines()
-exclude_list = map(str.strip, exclude_lines)
+raw_exclude_list = map(str.strip, exclude_lines)
+exclude_list = [d for d in raw_exclude_list if not d.startswith('#')]
 excludes = set(exclude_list)
 
 excluded_extensions_filename = "excluded_extensions.perg"
 excluded_extensions_filepath = "/".join([script_dir, excluded_extensions_filename])
 with open(excluded_extensions_filepath) as excluded_extensions_file :
     excluded_extensions_lines = excluded_extensions_file.readlines()
-excluded_extensions = map(str.strip, excluded_extensions_lines)
+raw_excluded_extensions = map(str.strip, excluded_extensions_lines)
+excluded_extensions =[d for d in raw_excluded_extensions if not d.startswith('#')] 
 
 #get standard directories to search
 default_search_directories_filename = "default_search_directories.perg"
@@ -36,18 +40,31 @@ search_directories_raw = default_search_directories + [arguments.directory]
 search_directories_fullpath = map(os.path.abspath, search_directories_raw)
 search_directories = list(set(search_directories_fullpath))
 
-def walk_folder(folder, searchterm):
-    print "Searching folder", folder
+zip_extensions = ['zip', 'jar', 'war']
+
+def walk_folder(folder, searchterm, includefirstline = True):
+    if includefirstline:
+        print "Searching folder", folder
     matchesFound = False
     for (dirpath, dirnames, filenames) in os.walk(folder):
         dirnames[:] = [d for d in dirnames if not (d in excludes)]
         for filename in filenames:
             dirs = dirpath.split("/")
             filename_parts = filename.split(".")
-#            pdb.set_trace()
+#           pdb.set_trace()
             if len(filename_parts) == 1 or filename_parts[-1] not in excluded_extensions:
+                if(filename_parts[-1] in zip_extensions:
+                  #unzip it
+                  zip_ref = zipfile.Zipfile(filename, 'r')
+                  unzipped_name = filename+"#" 
+                  zip_ref.extractall(unzipped_name)
+                  zip_ref.close()
+                  walk_folder(unzipped_name, searchterm, False)
+                  shutil.rmtree(unzipped_name) #delete after we're done
                 try:
                     filepath = "/".join([dirpath , filename])
+                    if is_binary(filepath):
+                        break
                     opened = open(filepath)
                     str_f = opened.read()
                     idx = str_f.find(searchterm)
@@ -58,6 +75,9 @@ def walk_folder(folder, searchterm):
                         matching_lines = [l for l in f_lines if l.find(searchterm) != -1]
                         for m in matching_lines:
                             print "    ", m
+                    else:
+                        if filename.find(searchterm) != -1:
+                            print filepath
                 except UnicodeDecodeError:
                     print "nontext file"
                 except IOError:
